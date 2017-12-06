e8c27056dec9061987a987c573aa19a4503d041b
David Braun <David@NodeGuy.com>
2017-12-06 14:38:07 -0500

    Add ESLint.

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es6: true,
+    mocha: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  rules: {
+    "no-unused-vars": "warn"
+  }
+};
